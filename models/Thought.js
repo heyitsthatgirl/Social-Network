51fa56697f714cb1d7a6f5e30f8c066d2939e288
hopemansfield <hopemansfield@gmail.com>
2023-04-21 12:26:17 -0400

    added reactionsSchema to Thoughts.js, still need to figure out how to make a getter to format the timestamp

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,50 @@
 const { Schema, model } = require("mongoose");
 
+const reactionsSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    //   * Use a getter method to format the timestamp on query
+    // get:
+  },
+});
+// **Reaction** (SCHEMA ONLY)
+
+// * `reactionId`
+//   * Use Mongoose's ObjectId data type
+//   * Default value is set to a new ObjectId
+
+// * `reactionBody`
+//   * String
+//   * Required
+//   * 280 character maximum
+
+// * `username`
+//   * String
+//   * Required
+
+// * `createdAt`
+//   * Date
+//   * Set default value to the current timestamp
+//   * Use a getter method to format the timestamp on query
+
+// **Schema Settings**:
+
+// This will not be a model, but rather will be used as the `reaction` field's subdocument schema in the `Thought` model.
+
 // create thought schema/model
 const thoughtSchema = new Schema(
   {
@@ -67,3 +112,27 @@ module.exports = Thought;
 // ---
 
 //reference: https://stackoverflow.com/questions/28829912/mongoose-schema-set-max-length-for-a-string#:~:text=Since%20Mongoose%204.0.,true%2C%20maxLength%3A%2020%7D%20%7D)%3B
+
+// **Reaction** (SCHEMA ONLY)
+
+// * `reactionId`
+//   * Use Mongoose's ObjectId data type
+//   * Default value is set to a new ObjectId
+
+// * `reactionBody`
+//   * String
+//   * Required
+//   * 280 character maximum
+
+// * `username`
+//   * String
+//   * Required
+
+// * `createdAt`
+//   * Date
+//   * Set default value to the current timestamp
+//   * Use a getter method to format the timestamp on query
+
+// **Schema Settings**:
+
+// This will not be a model, but rather will be used as the `reaction` field's subdocument schema in the `Thought` model.
